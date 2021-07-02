dec44c739a0da3fa49c629306703c1db62b66aa8
anhnguyet2305 <sonbnnth2007009@fpt.edu.vn>
2021-07-02 11:51:13 +0700

    aaaa

--- PracticalPHP/database/factories/ApartmentFactory.php
+++ PracticalPHP/database/factories/ApartmentFactory.php
@@ -25,10 +25,11 @@ class ApartmentFactory extends Factory
             //
             'name' => $this->faker->company,
             'address' => $this->faker->address,
-            'price' => $this->faker->numberBetween(1000000000, 10000000000),
+            'price' => $this->faker->numberBetween(1000, 10000000000),
             'general_info' => $this->faker->catchPhrase,
-            'details' => $this->faker->text(200),
-            'status' => $this->faker->numberBetween(0, 1)
+            'details' => $this->faker->text(250),
+            'status' => $this->faker->numberBetween(0, 1),
+            'image' => $this->faker->imageUrl(400, 200, 'apartments')
         ];
     }
 }
